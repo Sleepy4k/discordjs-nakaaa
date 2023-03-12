d4dcaeb28c62e43a4869e420c5c05cedfb49f330
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-03-12 17:53:41 +0700

    add polling in slash cmd

--- interaction/polling.js
+++ interaction/polling.js
@@ -2,17 +2,40 @@ const { EmbedBuilder } = require("discord.js");
 
 module.exports = {
   name: "polling",
-  cooldown: 2,
+  description: "Make polling.",
+  cooldown: 1,
+  options: [
+    {
+      name: "poll 1",
+      description: "Polling",
+      type: 3,
+      required: true,
+    },
+    {
+      name: "poll 2",
+      description: "Polling",
+      type: 3,
+      required: false,
+    },
+    {
+      name: "poll 3",
+      description: "Polling",
+      type: 3,
+      required: false,
+    },
+  ],
 
   /**
    * @param {import('discord.js').Client} client
-   * @param {import('discord.js').Message} message
+   * @param {import('discord.js').CommandInteraction} interaction
    */
-  exec: async (client, message, args) => {
-    let poll = args.join(" ");
+  exec: async (client, interaction) => {
+    let poll1 = interaction.options.getString("poll 1", true);
+    let poll2 = interaction.options.getString("poll 2", true);
+    let poll3 = interaction.options.getString("poll 3", true);
 
-    if (!poll) {
-      let emptyEmbed = new EmbedBuilder()
+    if (!poll1) {
+      let embed = new EmbedBuilder()
         .setTitle("woahh")
         .setColor("#fc2403")
         .setDescription(
@@ -22,13 +45,15 @@ module.exports = {
           text: `XII RPL 1 | Bot by Nakaaaa#8558`,
           iconURL:
             "https://cdn.discordapp.com/icons/1083339991331131392/495bb6b9a8bd90d2c09627ce2bec9a45.webp",
-        });
-      message.channel.send({ embeds: [emptyEmbed] }).then((msg) => {
+        })
+        .setTimestamp();
+
+      interaction.reply({ embeds: [embed] }).then((msg) => {
         setTimeout(() => {
           msg.delete();
         }, 5000);
       });
-      message.delete();
+      interaction.deleteReply();
     } else {
       let embed = new EmbedBuilder()
         .setColor("Random")
@@ -39,9 +64,10 @@ module.exports = {
           iconURL:
             "https://cdn.discordapp.com/icons/1083339991331131392/495bb6b9a8bd90d2c09627ce2bec9a45.webp",
         })
-        .setTimestamp(new Date());
-      message.channel
-        .send({ embeds: [embed] })
+        .setTimestamp();
+
+      interaction
+        .reply({ embeds: [embed] })
         .then((message) => {
           const sent = message;
           sent
@@ -52,7 +78,7 @@ module.exports = {
             .catch(console.error);
         })
         .catch(console.error);
-      return message.delete();
+      return interaction.deleteReply();
     }
   },
 };
