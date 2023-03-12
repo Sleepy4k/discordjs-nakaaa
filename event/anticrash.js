8b5146df83491ee9bdfbfe797149ad5176c60675
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-03-12 12:53:00 +0700

    upgrade to discord js 14

--- event/anticrash.js
+++ event/anticrash.js
@@ -1,125 +1,103 @@
 const { inspect } = require("util");
-const { MessageEmbed, WebhookClient } = require("discord.js");
-const s = new WebhookClient({
+const { EmbedBuilder, WebhookClient } = require("discord.js");
+const webhook = new WebhookClient({
   url: "https://discord.com/api/webhooks/1084337532105392148/cQsOCefThG_am0tZ4fKi9gcr3WhPBlrB347WYGsfrcz5iUv7dEzLUhT2f9wgO0MEXbXs",
 });
 
 module.exports = (client) => {
+  const embed = new EmbedBuilder().setColor("Red");
+
   client.on("error", (err) => {
     console.log(err);
-    const ErrorEmbed = new MessageEmbed()
-      .setTitle("Error")
-      .setURL("https://discordjs.guide/popular-topics/errors.html#api-errors")
-      .setColor("#2F3136")
-      .setDescription(`\`\`\`${inspect(error, { depth: 0 })}\`\`\``)
 
+    embed
+      .setTitle("Discord API Error")
+      .setURL("https://discordjs.guide/popular-topics/errors.html#api-errors")
+      .setDescription(
+        `\`\`\`${inspect(err, { depth: 0 }).slice(0, 1000)}\`\`\``
+      )
       .setTimestamp();
-    return s.send({
-      embeds: [ErrorEmbed],
-    });
+
+    return webhook.send({ embeds: [embed] });
   });
 
-  process.on("unhandledRejection", (reason, p) => {
-    console.log("——————————[Unhandled Rejection/Catch]——————————\n", reason, p);
-    const unhandledRejectionEmbed = new MessageEmbed()
-      .setTitle("**🟥 There was an Unhandled Rejection/Catch 🟥**")
+  process.on("unhandledRejection", (reason, promise) => {
+    console.log(reason, "\n", promise);
+
+    embed
+      .setTitle("Unhandled Rejection/Catch")
       .setURL("https://nodejs.org/api/process.html#event-unhandledrejection")
-      .setColor("RED")
-      .addField(
-        "Reason",
-        `\`\`\`${inspect(reason, { depth: 0 })}\`\`\``.substring(0, 1000)
-      )
-      .addField(
-        "Promise",
-        `\`\`\`${inspect(p, { depth: 0 })}\`\`\``.substring(0, 1000)
+      .addFields(
+        {
+          name: "Reason",
+          value: `\`\`\`${inspect(reason, { depth: 0 }).slice(0, 1000)}\`\`\``,
+        },
+        {
+          name: "Promise",
+          value: `\`\`\`${inspect(promise, { depth: 0 }).slice(0, 1000)}\`\`\``,
+        }
       )
       .setTimestamp();
-    return s.send({
-      embeds: [unhandledRejectionEmbed],
-    });
+
+    return webhook.send({ embeds: [embed] });
   });
 
   process.on("uncaughtException", (err, origin) => {
-    console.log(err, origin);
-    const uncaughtExceptionEmbed = new MessageEmbed()
-      .setTitle("**🟥There was an Uncaught Exception/Catch 🟥**")
-      .setColor("RED")
+    console.log(err, "\n", origin);
+
+    embed
+      .setTitle("Uncaught Exception/Catch")
       .setURL("https://nodejs.org/api/process.html#event-uncaughtexception")
-      .addField(
-        "Error",
-        `\`\`\`${inspect(err, { depth: 0 })}\`\`\``.substring(0, 1000)
-      )
-      .addField(
-        "Origin",
-        `\`\`\`${inspect(origin, { depth: 0 })}\`\`\``.substring(0, 1000)
+      .addFields(
+        {
+          name: "Error",
+          value: `\`\`\`${inspect(err, { depth: 0 }).slice(0, 1000)}\`\`\``,
+        },
+        {
+          name: "Origin",
+          value: `\`\`\`${inspect(origin, { depth: 0 }).slice(0, 1000)}\`\`\``,
+        }
       )
       .setTimestamp();
-    return s.send({
-      embeds: [uncaughtExceptionEmbed],
-    });
+
+    return webhook.send({ embeds: [embed] });
   });
 
   process.on("uncaughtExceptionMonitor", (err, origin) => {
-    console.log(err, origin);
-    const uncaughtExceptionMonitorEmbed = new MessageEmbed()
-      .setTitle("**🟥 There was an Uncaught Exception Monitor 🟥**")
-      .setColor("RED")
+    console.log(err, "\n", origin);
+
+    embed
+      .setTitle("Uncaught Exception Monitor")
       .setURL(
         "https://nodejs.org/api/process.html#event-uncaughtexceptionmonitor"
       )
-      .addField(
-        "Error",
-        `\`\`\`${inspect(err, { depth: 0 })}\`\`\``.substring(0, 1000)
-      )
-      .addField(
-        "Origin",
-        `\`\`\`${inspect(origin, { depth: 0 })}\`\`\``.substring(0, 1000)
-      )
-
-      .setTimestamp();
-
-    return s.send({
-      embeds: [uncaughtExceptionMonitorEmbed],
-    });
-  });
-
-  process.on("multipleResolves", (type, promise, reason) => {
-    console.log(type, promise, reason);
-    const multipleResolvesEmbed = new MessageEmbed()
-      .setTitle("**🟥 There was an Multiple Resolve 🟥**")
-      .setURL("https://nodejs.org/api/process.html#event-multipleresolves")
-      .setColor("RED")
-      .addField(
-        "Type",
-        `\`\`\`${inspect(type, { depth: 0 })}\`\`\``.substring(0, 1000)
-      )
-      .addField(
-        "Promise",
-        `\`\`\`${inspect(promise, { depth: 0 })}\`\`\``.substring(0, 1000)
-      )
-      .addField(
-        "Reason",
-        `\`\`\`${inspect(reason, { depth: 0 })}\`\`\``.substring(0, 1000)
+      .addFields(
+        {
+          name: "Error",
+          value: `\`\`\`${inspect(err, { depth: 0 }).slice(0, 1000)}\`\`\``,
+        },
+        {
+          name: "Origin",
+          value: `\`\`\`${inspect(origin, { depth: 0 }).slice(0, 1000)}\`\`\``,
+        }
       )
       .setTimestamp();
-    return s.send({
-      embeds: [multipleResolvesEmbed],
-    });
+
+    return webhook.send({ embeds: [embed] });
   });
 
   process.on("warning", (warn) => {
     console.log(warn);
-    const warningEmbed = new MessageEmbed()
-      .setTitle("**🟥 There was an Uncaught Exception Monitor Warning 🟥**")
-      .setColor("RED")
+
+    embed
+      .setTitle("Uncaught Exception Monitor Warning")
       .setURL("https://nodejs.org/api/process.html#event-warning")
-      .addField(
-        "Warn",
-        `\`\`\`${inspect(warn, { depth: 0 })}\`\`\``.substring(0, 1000)
-      )
+      .addFields({
+        name: "Warning",
+        value: `\`\`\`${inspect(warn, { depth: 0 }).slice(0, 1000)}\`\`\``,
+      })
       .setTimestamp();
-    return s.send({
-      embeds: [warningEmbed],
-    });
+
+    return webhook.send({ embeds: [embed] });
   });
 };
--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const config = require("./config");
 const { Player } = require("discord-player");
-const { Collection, Client, Intents } = require("discord.js");
+const { Collection, Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
   intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_VOICE_STATES,
-    Intents.FLAGS.GUILD_PRESENCES,
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.GuildPresences,
   ],
   ws: {
     properties: {
