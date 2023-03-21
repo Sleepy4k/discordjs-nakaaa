647bd08fc006a22129c2bc2a95ebf8bd9e3e38a3
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-03-21 10:58:12 +0700

    chore: index.js

--- index.js
+++ index.js
@@ -11,62 +11,8 @@
  *
  * March 12, 2023
  */
+import { Bot } from "./handlers/client.js";
 
-const config = require("./config");
-const { Player } = require("discord-player");
-const {
-  Client,
-  Partials,
-  Collection,
-  GatewayIntentBits,
-} = require("discord.js");
+export const client = new Bot();
 
-const client = new Client({
-  restTimeOffset: 0,
-  autoReconnect: true,
-  fetchAllMembers: true,
-  disabledEvents: ["TYPING_START"],
-  allowedMentions: {
-    parse: ["users", "roles"],
-    repliedUser: true,
-  },
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildVoiceStates,
-    GatewayIntentBits.GuildPresences,
-  ],
-  partials: [
-    Partials.User,
-    Partials.Channel,
-    Partials.Message,
-    Partials.Reaction,
-    Partials.GuildMember,
-    Partials.GuildScheduledEvent,
-  ],
-  ws: {
-    properties: {
-      $browser: config.options.browser,
-    },
-  },
-});
-
-client.prefix = config.options.prefix;
-client.commands = new Collection();
-client.aliases = new Collection();
-client.events = new Collection();
-client.errros = new Collection();
-cooldowns = new Collection();
-
-client.player = new Player(client, {
-  ytdlOptions: {
-    quality: "highestaudio",
-    highWaterMark: 1 << 25,
-  },
-});
-
-require("./event/handler.js").exec(client);
-require("./event/anticrash.js")(client);
-
-client.login(config.options.token);
+client.build(client.config.options.token);
