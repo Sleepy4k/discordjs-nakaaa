e7b76410425113a3c7cef610d7fccbd0e014cdab
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-03-25 19:04:32 +0700

    feat!: dashboard config

--- dashboard/config/app.config.js
+++
@@ -1,8 +0,0 @@
-import "dotenv/config.js";
-
-export const name = process.env.APP_NAME || "Express JS";
-export const env = process.env.APP_ENV || "development";
-export const port = process.env.APP_PORT || "3000";
-export const url = process.env.APP_URL || "http://localhost:3000";
-
-// Path: dashboard\config\app.config.js
