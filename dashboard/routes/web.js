3dd8665a9cd951757d15651437cc70254b4164ac
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-11-05 16:52:28 +0700

    chore: web route

--- dashboard/routes/web.js
+++ dashboard/routes/web.js
@@ -9,7 +9,7 @@ import { version } from "discord.js";
  */
 const router = Router();
 
-/* Api Route. */
+/* GET home page. */
 router.get("/", function (req, res, next) {
   res.status(200);
   res.render("pages/index", {
@@ -17,6 +17,22 @@ router.get("/", function (req, res, next) {
     node: process.version,
     title: req.app.get("client").config.web.name,
     author: req.app.get("client").config.bot.author,
+  });
+});
+
+/* GET docs page. */
+router.get("/docs", function (req, res, next) {
+  res.status(200);
+  res.render("pages/docs", {
+    title: req.app.get("client").config.web.name + " | Docs",
+  });
+});
+
+/* GET discord page. */
+router.get("/discord", function (req, res, next) {
+  res.status(200);
+  res.render("pages/discord", {
+    title: req.app.get("client").config.web.name + " | Discord",
   });
 });
 
