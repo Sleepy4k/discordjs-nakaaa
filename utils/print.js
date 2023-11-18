3d6e121aa8a49dd4cb65e2bc317b51b83d258dae
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-11-18 15:42:19 +0700

    chore(util): add type case

--- utils/print.js
+++ utils/print.js
@@ -16,15 +16,32 @@
  * Parse current date and time to human readable for console log with message
  *
  * @param {String} message
+ * @param {String} type - error, warn, debug, info, default
  *
  * @returns {void}
  */
-export default function print(message) {
+export default function print(message, type = "default") {
   const now = new Date();
   const date = now.toLocaleDateString();
   const time = now.toLocaleTimeString();
 
-  console.log(`[${date} ${time}] ${message}`);
+  switch (type.toLowerCase()) {
+    case "error":
+      console.log(`[${date} ${time}] [ERROR] ${message}`);
+      break;
+    case "warn":
+      console.log(`[${date} ${time}] [WARN] ${message}`);
+      break;
+    case "debug":
+      console.log(`[${date} ${time}] [DEBUG] ${message}`);
+      break;
+    case "info":
+      console.log(`[${date} ${time}] [INFO] ${message}`);
+      break;
+    default:
+      console.log(`[${date} ${time}] ${message}`);
+      break;
+  }
 }
 
 // Path: utils\parseDur.js
